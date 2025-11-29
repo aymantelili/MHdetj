a8f4890317d0030895a046b041a9964bf3c9bc54
aymantelili <ayman.telili@gmail.com>
2025-11-29 20:30:39 +0100

    Create bgFilesList.jsx

---
+++ src/data/bgFilesList.jsx
@@ -0,0 +1,14 @@
+export const bgFilesMap = {
+  INR: {
+    Ligne1: ["INR1.xlsx", "INR2.xlsx"],
+    Ligne2: ["INR3.xlsx"]
+  },
+  COC: {
+    Ligne1: ["COC1.xlsx"],
+    Ligne2: ["COC2.xlsx"]
+  },
+  BASIS: {
+    Ligne1: ["BASIS1.xlsx"],
+    Ligne3: ["BASIS3.xlsx"]
+  }
+};
