cb16583fcc7ddfa104da8c27a1242525d645c145
aymantelili <ayman.telili@gmail.com>
2025-11-29 20:28:52 +0100

    Create bgDatabase.js

---
+++ src/data/bgDatabase.js
@@ -0,0 +1,24 @@
+// Mock data structure example
+export const bgDatabase = [
+  {
+    cable: "AJ700001",
+    location: {
+      area: "VOR",
+      location: "VOR1.1",
+      standar: 1,
+      level: 3,
+      shelf: 5
+    }
+  },
+  {
+    cable: "AJ712345",
+    location: {
+      area: "TAKT",
+      location: "TAKT5_uss",
+      standar: 2,
+      level: 16,
+      shelf: 4
+    }
+  },
+  // more entries...
+];
