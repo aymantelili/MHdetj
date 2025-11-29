e203c3537556657e024d3acc0d70fa6f11e96e91
aymantelili <ayman.telili@gmail.com>
2025-11-29 21:29:31 +0100

    Update Tailwind config file paths

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./MHdetj/index.html', './MHdetj/src/**/*.{js,jsx}'],
+  content: ['./index.html', './src/**/*.{js,jsx}'],
   theme: {
     extend: {},
   },
   plugins: [],
 };
+
 
